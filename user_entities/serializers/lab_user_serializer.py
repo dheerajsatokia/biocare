f3dc3a6404c1c9a1dab6ced4eb7f164ba325187c
ayush3298 <abairagi311@gmail.com>
2020-04-02 05:21:25 +0530

    added address to lab

--- user_entities/serializers/lab_user_serializer.py
+++ user_entities/serializers/lab_user_serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from ..models import LabUser
 from user.serializer import AuthSerializer
 from rest_framework.validators import UniqueValidator
-from user.models import User
+from user.models import User, Address
 
 
 class LabUserSerializer(serializers.ModelSerializer):
@@ -24,6 +24,12 @@ class LabUserPostSerializer(serializers.ModelSerializer):
     last_name = serializers.CharField(max_length=50, required=True)
     mobile_number = serializers.IntegerField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
 
+    address1 = serializers.CharField(max_length=1024)
+    address2 = serializers.CharField(max_length=1024)
+    zip_code = serializers.CharField(max_length=12)
+    city = serializers.CharField(max_length=12)
+    country = serializers.CharField(max_length=250)
+
     class Meta:
         model = LabUser
         fields = '__all__'
@@ -34,6 +40,14 @@ class LabUserPostSerializer(serializers.ModelSerializer):
                                    last_name=validated_data['last_name'], mobile_number=validated_data['mobile_number'])
         user.set_password(validated_data['password'])
         user.save()
+
+        Address.objects.create(user=user,
+                               address1=validated_data['address1'],
+                               address2=validated_data['address2'],
+                               zip_code=validated_data['zip_code'],
+                               city=validated_data['city'],
+                               country=validated_data['country'],
+                               )
         lab_user = LabUser.objects.create(user=user)
         return lab_user
 
@@ -44,9 +58,16 @@ class LabUserPutSerializer(serializers.ModelSerializer):
     last_name = serializers.CharField(max_length=50, required=True)
     mobile_number = serializers.IntegerField(required=True)
 
+    address1 = serializers.CharField(max_length=1024)
+    address2 = serializers.CharField(max_length=1024)
+    zip_code = serializers.CharField(max_length=12)
+    city = serializers.CharField(max_length=12)
+    country = serializers.CharField(max_length=250)
+
     class Meta:
         model = LabUser
-        fields = ['id', 'email', 'first_name', 'last_name', 'mobile_number', 'kyc_lab1', 'kyc_lab2', 'is_kyc_approved']
+        fields = ['id', 'email', 'first_name', 'last_name', 'mobile_number', 'kyc_lab1', 'kyc_lab2', 'is_kyc_approved',
+                  'address1', 'address2', 'zip_code', 'city', 'country']
 
     def update(self, instance, validated_data):
         user = instance.user
@@ -55,6 +76,15 @@ class LabUserPutSerializer(serializers.ModelSerializer):
         user.last_name = validated_data.get('last_name')
         user.mobile_number = validated_data.get('mobile_number')
         user.save()
+
+        address = user.address
+        address.address1 = validated_data.get('address1')
+        address.address2 = validated_data.get('address2')
+        address.zip_code = validated_data.get('zip_code')
+        address.city = validated_data.get('city')
+        address.country = validated_data.get('country')
+        address.save()
+
         instance.kyc_lab1 = validated_data.get('kyc_lab1')
         instance.kyc_lab2 = validated_data.get('kyc_lab2')
         instance.is_kyc_approved = validated_data.get('is_kyc_approved')
