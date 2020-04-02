399ebbc20cbbc286f7c39fa7f7d13b49194ccbe7
ayush3298 <abairagi311@gmail.com>
2020-04-02 23:38:29 +0530

    added approval pending

--- user_entities/services/dashboard_service.py
+++ user_entities/services/dashboard_service.py
@@ -2,6 +2,8 @@ from ..serializers import dashboard_serializer
 from rest_framework.response import Response
 from rest_framework import status
 from ..models import Doctor, Chemist, LabUser
+from ..serializers import doctor_serializers, chemist_serializer, lab_user_serializer
+import json
 
 
 def get_count():
@@ -14,10 +16,27 @@ def get_count():
         return ser.data
 
 
+def doctor_pending_approval():
+    doctors = Doctor.objects.filter(is_kyc_approved=False)
+    doctors = doctor_serializers.DoctorSerializer(doctors, many=True).data
+
+    chemist_obj = Chemist.objects.filter(is_kyc_approved=False)
+    chemist = chemist_serializer.ChemistSerializer(chemist_obj, many=True).data
+
+    labs = LabUser.objects.filter(is_kyc_approved=False)
+    labs = lab_user_serializer.LabUserSerializer(labs, many=True).data
+
+    data = {'pending_approvals': {
+        'doctors': doctors,
+        'chemist': chemist,
+        'labs': labs
+    }}
+    json_str = json.dumps(data)
+    json_data = json.loads(json_str)
+    return json_data
+
+
 def get_dashboard():
     count = get_count()
-    data = {'counts': count}
-    ser = dashboard_serializer.DashBoardSerializer(data=data)
-    if ser.is_valid():
-        return Response(ser.data, status=status.HTTP_200_OK)
-    return Response(ser.errors)
+    data = {'counts': count, 'pending_approvals': doctor_pending_approval()}
+    return Response(data)
